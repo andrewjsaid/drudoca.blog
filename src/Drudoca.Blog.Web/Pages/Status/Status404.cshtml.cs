ecfc19aad73b2f3b689e8bda6737d0f2a550cd83
Andrew J Said <andrewjsaid@gmail.com>
2020-06-07 19:07:47 +0100

    Do not specify layout needlessly

--- src/Drudoca.Blog.Web/Pages/Status/Status404.cshtml.cs
+++ src/Drudoca.Blog.Web/Pages/Status/Status404.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Drudoca.Blog.Web.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Drudoca.Blog.Web.Pages.Status
 {
+    [LayoutModel]
     public class Status404Model : PageModel
     {
         public void OnGet()
